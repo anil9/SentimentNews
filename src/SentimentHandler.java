6ac810625131f9d2028202de81def701eb4e55e5
Andreas Nilsson <anil9@kth.se>
2017-04-06 16:12:32 +0200

    minor changes to sentiment

--- src/SentimentHandler.java
+++ src/SentimentHandler.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SentimentHandler {
@@ -13,14 +14,16 @@ public class SentimentHandler {
 		this.tagger = tagger;
 	}
 
-	public List<Article> getPositiveArticles() {
+	public List<Article> getPositiveArticles(double positiveThreshold) {
 		List<Article> allArticles = content.getArticles();
 		List<Article> positiveArticles = new ArrayList<Article>();
 		for (Article article : allArticles) {
-			List<String> adjectives = tagger.extractAdjectives(article.getHeader());
-			// adjectives.forEach((adjective) -> System.out.println(adjective));
-			int positives = sentiment.calcNumPositive(adjectives);
-			if (positives > 0) {
+			// List<String> adjectives = tagger.extractAdjectives(article.getHeader());
+			List<String> allWords = Arrays.asList(article.getHeader().split(" "));
+			double percentPositive = sentiment.calcPositivePercentage(allWords);
+			// double percentPositive = sentiment.calcPositivePercentage(adjectives); // with adjectives
+			// System.out.println("percentPositive was: " + percentPositive);
+			if (percentPositive >= positiveThreshold) {
 				positiveArticles.add(article);
 			}
 		}
@@ -31,12 +34,13 @@ public class SentimentHandler {
 	public static void main(String[] args) {
 		// Sample setup
 
-		Content content = new Content("reddit-r-all");
+		double positiveThreshold = 0.5;
+		Content content = new Content("hacker-news");
 		// System.out.println("All headlines:");
 		// content.getArticles().forEach((article) -> System.out.println(article.getHeader()));
 		System.out.println("Headlines with positive sentiment:");
 		List<Article> positiveArticles = new SentimentHandler(new Sentiment(), content, new POSTagger())
-				.getPositiveArticles();
+				.getPositiveArticles(positiveThreshold);
 		positiveArticles.forEach((article) -> System.out.println(article.getHeader()));
 	}
 }
